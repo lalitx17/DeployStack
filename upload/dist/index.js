da4d236a91204d254625ee79cfed7a1078e9509a
Lalit Yadav <lalityadav.x17@gmail.com>
2024-06-26 13:39:35 -0500

    s3 bucket added

--- upload/dist/index.js
+++ upload/dist/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,17 +38,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const idGenerator_1 = require("./idGenerator");
+const getAllFiles_1 = require("./getAllFiles");
 const simple_git_1 = require("simple-git");
+const client_s3_1 = require("@aws-sdk/client-s3");
+const path_1 = __importDefault(require("path"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const client = new client_s3_1.S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    },
+});
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+function listBuckets() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const command = new client_s3_1.ListBucketsCommand({});
+            const response = yield client.send(command);
+            console.log("Buckets:", response.Buckets);
+        }
+        catch (error) {
+            console.error("Error listing buckets:", error);
+        }
+    });
+}
+listBuckets();
 app.post("/deploy", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const repoUrl = req.body.repoUrl;
     const id = (0, idGenerator_1.idGenerator)(5);
-    yield (0, simple_git_1.simpleGit)().clone(repoUrl, `output/${id}`);
+    yield (0, simple_git_1.simpleGit)().clone(repoUrl, path_1.default.join(__dirname, `output/${id}`));
     res.json({
         id: id
     });
+    const files = (0, getAllFiles_1.getAllFiles)(path_1.default.join(__dirname, `output/${id}`));
 }));
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
--- upload/src/index.ts
+++ upload/src/index.ts
@@ -1,21 +1,53 @@
 import express from "express";
 import cors from "cors";
 import { idGenerator } from "./idGenerator";
+import { getAllFiles } from "./getAllFiles";
 import {simpleGit} from "simple-git";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import path from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    },
+  });
+  
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
+async function listBuckets() {
+    try {
+      const command = new ListBucketsCommand({});
+      const response = await client.send(command);
+      console.log("Buckets:", response.Buckets);
+    } catch (error) {
+      console.error("Error listing buckets:", error);
+    }
+  }
+  
+  listBuckets();
+
+  
 app.post("/deploy", async(req, res) => {
     const repoUrl = req.body.repoUrl;   
     const id = idGenerator(5);  
-    await simpleGit().clone(repoUrl, `output/${id}`);
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
     res.json({
         id: id
     })
+
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+
+
+
 })
 
 
