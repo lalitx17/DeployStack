e4ce479c6abb079e8566678f2eb505f12f5a111c
Lalit Yadav <lalityadav.x17@gmail.com>
2024-06-27 12:40:44 -0500

    id retrieved from sqs in deploy

--- deploy/src/index.ts
+++ deploy/src/index.ts
@@ -1,1 +1,50 @@
-console.log("Hello World");
+import {
+  SQSClient,
+  ReceiveMessageCommand,
+  DeleteMessageCommand,
+} from "@aws-sdk/client-sqs";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const sqsClient = new SQSClient({
+  region: process.env.AWS_REGION,
+});
+
+const queueURL = process.env.AWS_QUEUE_URL;
+
+const infinitelyReceiveMessages = async () => {
+  while (true) {
+    try {
+      const receiveParams = {
+        QueueUrl: queueURL,
+        MaxNumberOfMessages: 1,
+        WaitTimeSeconds: 20,
+      };
+
+      const data = await sqsClient.send(
+        new ReceiveMessageCommand(receiveParams)
+      );
+      if (data.Messages && data.Messages.length > 0) {
+        for (const message of data.Messages) {
+          console.log("Received message: ", message.Body);
+
+          const deleteParams = {
+            QueueUrl: queueURL, 
+            ReceiptHandle: message.ReceiptHandle,
+          }
+
+          await sqsClient.send(new DeleteMessageCommand(deleteParams));
+          console.log("Message Processed and deleted");
+        }
+      }else {
+        console.log("No message received.")
+      }
+    } catch (err) {
+      console.error("Error receiving messages: ", err);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+};
+
+
+infinitelyReceiveMessages();
